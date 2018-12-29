9288007d8c5ec3315ea96e2d1a5b79ae40f230be
Berlingrad <stormrisexu@hotmail.com>
2018-12-29 09:07:24 +0800

    add Identification and recording

--- TranscriptionAndIdentificaiton.py
+++ TranscriptionAndIdentificaiton.py
@@ -1,7 +1,20 @@
 import tkinter as tk                # python 3
 from tkinter import font  as tkfont # python 3
+import pyaudio, wave
 #import Tkinter as tk     # python 2
 #import tkFont as tkfont  # python 2
+
+speakerProfiles = {'7f459648-ee2e-4903-91b4-0145a1bafb88':'grace'}
+
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 16000
+CHUNK = 1024
+RECORD_SECONDS = 5
+
+Audio = pyaudio.PyAudio()
+stream = None
+
 
 class TIApp(tk.Tk):
 
@@ -10,7 +23,7 @@ class TIApp(tk.Tk):
 
         self.title("Transcription and Identification")
         self.title_font = tkfont.Font(family='Helvetica', size=18, weight="bold", slant="italic")
-        self.geometry("768x432")
+        self.geometry("1024x576")
 
         # the container is where we'll stack a bunch of frames
         # on top of each other, then the one we want visible
@@ -49,10 +62,10 @@ class StartPage(tk.Frame):
 
         button1 = tk.Button(self, text="Add a New Speaker Profile",
                             command=lambda: controller.show_frame("AddProfile"),
-                            width = 50)
+                            width = 70)
         button2 = tk.Button(self, text="Process an Audio",
                             command=lambda: controller.show_frame("Identify"),
-                            width = 50)
+                            width = 70)
         button1.pack()
         button2.pack()
 
@@ -60,13 +73,52 @@ class StartPage(tk.Frame):
 class AddProfile(tk.Frame):
 
     def __init__(self, parent, controller):
+
+        def _createProfile():
+            name = nameEntry.get()
+            nameEntry.delete(0, 'end')
+        def _startRecord():
+            stream = Audio.open(format=FORMAT, channels=CHANNELS,
+                                rate=RATE, input=True,
+                                frames_per_buffer=CHUNK)
+        def _stopRecord():
+            stream.stop_stream()
+            stream.close()
+            Audio.terminate()
+
+
+
+
+
         tk.Frame.__init__(self, parent)
         self.controller = controller
         label = tk.Label(self, text="Add a new speaker profile", font=controller.title_font)
         label.pack(side="top", fill="x", pady=10)
-        button = tk.Button(self, text="Go back to Main Menu",
+
+        nameLabel =tk.Label(self, text="Name:")
+        nameLabel.pack(side='top')
+
+        nameEntry = tk.Entry(self)
+        nameEntry.pack(side='top')
+
+
+        recordButton = tk.Button(self, text="Record", width=50,
+                                 command=_startRecord)
+        recordButton.pack(side='left')
+
+        stopButton = tk.Button(self, text="Stop", width=50,
+                               command=_stopRecord)
+        stopButton.pack(side='right')
+
+        createButton = tk.Button(self, text="Create Profile", width=50,
+                                 command=_createProfile)
+        createButton.pack(side='bottom')
+
+        button = tk.Button(self, text="Go Back to Main Menu", width=50,
                            command=lambda: controller.show_frame("StartPage"))
-        button.pack()
+        button.pack(side='bottom')
+
+
 
 
 class Identify(tk.Frame):
@@ -76,10 +128,29 @@ class Identify(tk.Frame):
         self.controller = controller
         label = tk.Label(self, text="Audio Processing", font=controller.title_font)
         label.pack(side="top", fill="x", pady=10)
-        button = tk.Button(self, text="Go back to Main Menu",
-                           command=lambda: controller.show_frame("StartPage"))
-        button.pack()
+       
+        sb = tk.Scrollbar(self)
+        display = tk.Text(self)
+        sb.pack(side='right', fill='y')
+        display.pack(side='left', fill= 'x')
+        sb.config(command=display.yview)
+        display.config(yscrollcommand=sb.set)
+        display.insert('end', "Upload a wav file to Start \n (Encoding: \t PCM Sample rate: 16k \t Sample format: 16 bit \t Chanel: Mono) ")
+        display.config(state='disabled')
+        display.pack(fill = "x", ipady=10)
 
+
+        button = tk.Button(self, text="Go Back to Main Menu", width = 20,
+                          command=lambda: controller.show_frame("StartPage"))
+        button.pack(side='bottom', fill = "y", pady = 10)
+
+
+
+        importButton = tk.Button(self, text="Import a Audio file", width=20)
+        importButton.pack(side='top', fill ='y', pady = 10)
+
+        processButton = tk.Button(self, text="Process", width=20)
+        processButton.pack(side='top', fill='y', pady=10)
 
 if __name__ == "__main__":
     app = TIApp()
